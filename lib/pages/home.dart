f76f6934b083a79fbf99238b06b169bbb4bb9ce7
M Rifqi <muhamadrifqi.id@gmail.com>
2025-04-16 16:51:24 +0700

    types

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,10 +1,20 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
+class SecondRoute extends StatelessWidget {
+  const SecondRoute({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(title: const Text("Halaman 2")),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: const Text('Kembali'),
+        ),
+      ),
+    );
   }
 }
