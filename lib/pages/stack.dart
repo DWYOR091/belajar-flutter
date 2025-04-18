8278269accff058f6763dba0d3d93812aa2dced9
M Rifqi <muhamadrifqi.id@gmail.com>
2025-04-18 12:57:07 +0700

    menambahkan icon pada appbar

--- lib/pages/stack.dart
+++ lib/pages/stack.dart
@@ -8,25 +8,37 @@ class CobaStack extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
           title: const Text("Menggunakan Stack Widget"),
+          backgroundColor: Colors.indigo[100],
+          leading: const Icon(
+            Icons.notification_add,
+            color: Colors.white,
+            shadows: [BoxShadow(blurRadius: 10)],
+            textDirection: TextDirection.rtl,
+          ),
         ),
-        body: Stack(
-          children: [
-            Image.asset(
-              "assets/images/lake.jpg",
-            ),
-            const SizedBox(
-              height: 380,
-              child: Center(
-                child: Text(
-                  "Flutter Stack Widget",
-                  style: TextStyle(fontSize: 40, color: Colors.white),
-                ),
+        body: Container(
+          padding: const EdgeInsets.all(20),
+          child: Stack(
+            children: [
+              Image.asset(
+                "assets/images/lake.jpg",
               ),
-            )
-          ],
+              const SizedBox(
+                //sizebox agar bisa nambah heigh dan width
+                height: 380,
+                child: Center(
+                  child: Text(
+                    "Flutter Stack Widget",
+                    style: TextStyle(fontSize: 40, color: Colors.white),
+                  ),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
