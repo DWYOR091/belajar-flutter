0646d530525b1593e0e9bfae330df7f9282c1982
M Rifqi <muhamadrifqi.id@gmail.com>
2025-04-22 19:58:53 +0700

    textfield

--- lib/views/pages/profile_page.dart
+++ lib/views/pages/profile_page.dart
@@ -1,12 +1,52 @@
 import 'package:flutter/material.dart';
 
-class ProfilePage extends StatelessWidget {
+class ProfilePage extends StatefulWidget {
   const ProfilePage({super.key});
 
   @override
+  State<ProfilePage> createState() => _ProfilePageState();
+}
+
+class _ProfilePageState extends State<ProfilePage> {
+  final TextEditingController textController1 = TextEditingController();
+  @override
+  void initState() {
+    textController1.addListener(
+      () {
+        print("Changed: ${textController1.text}");
+      },
+    );
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    textController1.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text("Profile Page"),
+    return Padding(
+      padding: const EdgeInsets.all(20.0),
+      child: Column(
+        children: [
+          TextField(
+            controller: textController1,
+            decoration: InputDecoration(
+                label: Text("Label"),
+                hintText: "Masukan text!",
+                border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(25))),
+            onEditingComplete: () {
+              setState(() {});
+            },
+          ),
+          Center(
+            child: Text(textController1.text),
+          )
+        ],
+      ),
     );
   }
 }
