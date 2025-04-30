63d06a95d65930c67814c28fbe812686abac8736
M Rifqi <muhamadrifqi.id@gmail.com>
2025-04-30 16:33:48 +0700

    card

--- lib/views/pages/home_page.dart
+++ lib/views/pages/home_page.dart
@@ -6,10 +6,62 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Column(
-          children: [HeroWidget()],
-        ));
+    return SingleChildScrollView(
+      child: Padding(
+          padding: const EdgeInsets.all(50.0),
+          child: Column(
+            children: [
+              HeroWidget(),
+              Container(
+                width: double.infinity,
+                padding: EdgeInsets.all(10.0),
+                child: Card(
+                  shadowColor: Colors.red,
+                  color: Colors.amber,
+                  child: Padding(
+                    padding: EdgeInsets.all(20.0),
+                    child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text("Judul"),
+                          Text("deksripsi"),
+                        ]),
+                  ),
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.all(10.0),
+                child: ListTile(
+                  leading: Icon(Icons.album, size: 50),
+                  title: Text('Judul Card'),
+                  subtitle: Text('Subtitle atau deskripsi singkat'),
+                  trailing: Icon(Icons.more_vert),
+                  onTap: () {
+                    print('Card ditekan');
+                  },
+                ),
+              ),
+              SizedBox(
+                height: 100.0,
+                child: ListView(
+                  padding: EdgeInsets.all(8.0),
+                  children: [
+                    for (int i = 1; i <= 5; i++)
+                      Card(
+                        margin: EdgeInsets.only(bottom: 10.0),
+                        child: ListTile(
+                          title: Text('Item $i'),
+                          subtitle: Text('Deskripsi untuk item $i'),
+                          onTap: () {
+                            print('Item $i ditekan');
+                          },
+                        ),
+                      ),
+                  ],
+                ),
+              )
+            ],
+          )),
+    );
   }
 }
