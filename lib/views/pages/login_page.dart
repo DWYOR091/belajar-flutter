605e804d6006584416dfce97c0340bacea0faa41
M Rifqi <muhamadrifqi.id@gmail.com>
2025-05-01 10:19:14 +0700

    library lottie

--- lib/views/pages/login_page.dart
+++ lib/views/pages/login_page.dart
@@ -1,6 +1,6 @@
 import 'package:belajar_flutter/views/widget_tree.dart';
-import 'package:belajar_flutter/views/widgets/hero_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -17,43 +17,52 @@ class _LoginPageState extends State<LoginPage> {
       //   title: Text("Login"),
       // ),
       body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(25.0),
-          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-            HeroWidget(),
-            Container(
-              margin: const EdgeInsets.only(top: 10.0),
-              child: TextField(
-                decoration: InputDecoration(
-                    labelText: "Username",
-                    hintText: "Masukan Username!",
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(25)))),
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(25.0),
+            child:
+                Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+              // HeroWidget(),
+              Text("Silahkan Login",
+                  style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold)),
+              SizedBox(
+                  height: 300,
+                  width: double.infinity,
+                  child: Lottie.asset("assets/lotties/login.json")),
+              Container(
+                margin: const EdgeInsets.only(top: 10.0),
+                child: TextField(
+                  decoration: InputDecoration(
+                      labelText: "Username",
+                      hintText: "Masukan Username!",
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(25)))),
+                ),
               ),
-            ),
-            Container(
-              margin: EdgeInsets.only(top: 10.0),
-              child: TextField(
-                decoration: InputDecoration(
-                    labelText: "Password",
-                    hintText: "Masukan Password!",
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(25)))),
+              Container(
+                margin: EdgeInsets.only(top: 10.0),
+                child: TextField(
+                  decoration: InputDecoration(
+                      labelText: "Password",
+                      hintText: "Masukan Password!",
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(Radius.circular(25)))),
+                ),
               ),
-            ),
-            Container(
-              margin: EdgeInsets.only(top: 10.0),
-              child: ElevatedButton(
-                  onPressed: () {
-                    Navigator.pushReplacement(context, MaterialPageRoute(
-                      builder: (context) {
-                        return WidgetTree();
-                      },
-                    ));
-                  },
-                  child: Text("Login")),
-            )
-          ]),
+              Container(
+                margin: EdgeInsets.only(top: 10.0),
+                child: ElevatedButton(
+                    onPressed: () {
+                      Navigator.pushReplacement(context, MaterialPageRoute(
+                        builder: (context) {
+                          return WidgetTree();
+                        },
+                      ));
+                    },
+                    child: Text("Login")),
+              )
+            ]),
+          ),
         ),
       ),
     );
